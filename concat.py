fb08a9102956b0f8c4f4e6a64c35060c243e6c86
vknanavati <107810028+vknanavati@users.noreply.github.com>
2022-06-22 11:58:21 -0700

    Add files via upload

---
+++ concat.py
@@ -0,0 +1,8 @@
+# string concatenation (aka how to put strings together)
+# suppose we want to create a string that says "subscribe to ____"
+youtuber = ""  # some string variable
+
+# a few ways to do this
+print("subscirbe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}")
